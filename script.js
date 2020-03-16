a7f88b31a1c4e854f7754982f96c3b7fa87c7603
Daniel Abell <da450@cairn.edu>
2020-03-15 21:55:38 -0400

    added localStorage functionality to click event populate city list with last searched city upon revisiting the page

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var citySearch;
 
+if (localStorage.savedCity !== null) {
+    var lastCity = $("<li>").text(localStorage.savedCity);
+    lastCity.attr("class", "list-group-item");
+    $("#city-list").append(lastCity);
+}
+
 $("button").on("click", function() {
     event.preventDefault();
     citySearch = $(this).prev().val();
+    if (citySearch === "") {
+        return;
+    }
+    localStorage.clear();
     var newCity = $("<li>").text(citySearch);
     newCity.attr("class", "list-group-item");
     $("#city-list").append(newCity);
-    localStorage.setItem("citySearches", citySearch);
+    localStorage.setItem("savedCity", citySearch);
     console.log(localStorage);
-})
+});
+
+$("li").on("click", function() {
+    event.preventDefault();
+    console.log(event.target);
+});
 
 $("#current-city").html(`Atlanta (8/15/2019)`);
 $("#temperature").html(`Temperature: `);
